140d6b001ea0a838884f6c8c596072522b538829
ntdat <ntdat19122002@gmail.com>
2023-03-05 23:37:51 +0700

    take json

--- bundle/controllers/main.py
+++ bundle/controllers/main.py
@@ -1,5 +1,4 @@
 import os
-
 import jinja2
 
 import odoo
@@ -9,11 +8,17 @@ import json
 path = os.path.realpath(os.path.join(os.path.dirname(__file__), '../static/html'))
 loader = jinja2.FileSystemLoader(path)
 jinja_env = jinja2.Environment(loader=loader, autoescape=True)
-# jinja_env.filters["json"] = json.dumps
+jinja_env.filters["json"] = json.dumps
 class BundleAPI(odoo.http.Controller):
     @odoo.http.route('/bundle', auth='public', type="http")
     def shop_bundle(self):
-        template = jinja_env.get_template('app.html')
-        products = request.env['product.product'].search([])
-        res = template.render(products=products)
+        template = jinja_env.get_template('index.html')
+        res = template.render()
         return res
+
+    @odoo.http.route('/bundle/api', auth='public', type="http")
+    def shop_bundle_api(self):
+        products = request.env['product.product'].search([])
+        for product in products:
+            print(product.name)
+        return json.dumps({'pruducts[0]':'asdf'})
